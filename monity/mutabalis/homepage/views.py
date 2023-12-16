d170aecd94de0d628050152625c696e18e706742
Gaguchi <gaga9393@gmail.com>
2023-12-16 14:58:12 +0400

    django

--- monity/mutabalis/homepage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'homepage/home.html')
-
-def projects(request):
-    return render(request, 'homepage/projects.html')
-
-# Create your views here.
