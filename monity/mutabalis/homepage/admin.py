99959f5c872c0fad6bea9d1b32c181cfb308d85c
Gaguchi <gaga9393@gmail.com>
2023-12-17 01:05:51 +0400

    DJANGO INLINE

--- monity/mutabalis/homepage/admin.py
+++ monity/mutabalis/homepage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-from .models import Project
-admin.site.register(Project)
+from .models import Project, Service, Step
+
+class ServiceInline(admin.TabularInline):
+    model = Service
+
+class StepInline(admin.TabularInline):
+    model = Step
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [ServiceInline, StepInline]
+
+admin.site.register(Project, ProjectAdmin)
--- monity/mutabalis/homepage/models.py
+++ monity/mutabalis/homepage/models.py
@@ -3,9 +3,26 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    services = models.CharField(max_length=200)
     how_we_work = models.TextField()
-    steps = models.TextField()
 
     def __str__(self):
         return self.title
+
+
+class Service(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)  # Add ForeignKey field
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Step(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    description = models.TextField()
+    # Add other fields as needed
+
+    def __str__(self):
+        return f"Step {self.number} of {self.project.title}"
