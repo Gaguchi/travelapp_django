4c469392fa6e4d53e7700e243ef33e040bbb4a79
Gaguchi <gaga9393@gmail.com>
2023-12-18 00:32:14 +0400

    api

--- monity/mutabalis/homepage/urls.py
+++ monity/mutabalis/homepage/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.projects, name='projects'),
+    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('projects/home/', views.projects_home, name='projects_home'),
 ]
--- monity/mutabalis/homepage/views.py
+++ monity/mutabalis/homepage/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'homepage/home.html')
 
 def projects(request):
     projects = Project.objects.all()
-    return render(request, 'homepage/projects.html', {'projects': projects})
+    project_list = list(projects.values())  # Convert QuerySet to list of dicts
+    return JsonResponse(project_list, safe=False)
 
-# Create your views here.
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    project_data = {
+        'id': project.id,
+        'name': project.title,
+        # Add more fields as needed
+    }
+    return JsonResponse(project_data)
+
+def projects_home(request):
+    return render(request, 'homepage/projects.html')
