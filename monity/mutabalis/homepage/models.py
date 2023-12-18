cee9e4d5b89e76ea5466ed3deb775dd41851888c
Gaguchi <gaga9393@gmail.com>
2023-12-18 23:30:27 +0400

    Add new fields to Project and Service models

--- monity/mutabalis/homepage/models.py
+++ monity/mutabalis/homepage/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
+    short_description = models.TextField()
     description = models.TextField()
     how_we_work = models.TextField()
+    banner_image = models.ImageField(upload_to='img/content/', null=True)
+    main_image = models.ImageField(upload_to='img/content/', null=True)
+    secondary_image = models.ImageField(upload_to='img/content/', null=True)
 
     def __str__(self):
         return self.title
@@ -13,6 +17,7 @@ class Service(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)  # Add ForeignKey field
     title = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='img/content/', null=True)
 
     def __str__(self):
         return self.title
